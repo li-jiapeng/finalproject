ea5c605604619170f8a32811ac5649e170b34827
Jiapeng Li <li.jiap@husky.neu.edu>
2020-04-18 15:44:19 -0400

    login page

--- webapp/src/app/login/login.component.ts
+++ webapp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../login-service.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginService: LoginServiceService;
+  td: any;
+  
+  constructor(loginService: LoginServiceService) {
+    this.loginService = loginService;
+  }
 
   ngOnInit(): void {
   }
